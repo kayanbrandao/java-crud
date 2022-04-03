57e04032679fa4036c78ad1d839e1b045fe5f367
kayanbrandao <kayanbrandao.krb@gmail.com>
2022-04-03 16:31:16 -0400

    update project

---
+++ src/crud/Delete.java
@@ -0,0 +1,20 @@
+package crud;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import connections.FabricaConexao;
+
+public class Delete {
+    public static void main(String[] args) throws SQLException{
+        Connection conexao = FabricaConexao.getConnection();
+
+        String sql = "DELETE FROM Pessoa WHERE Cod = ?";
+
+        PreparedStatement ps = conexao.prepareStatement(sql);
+        ps.setInt(1, 2);
+        ps.executeUpdate();
+        conexao.close();
+    }
+}
---
+++ src/crud/Update.java
@@ -0,0 +1,25 @@
+package crud;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import connections.FabricaConexao;
+import objects.Pessoa;
+
+public class Update {
+    public static void main(String[] args) throws SQLException{
+        Pessoa p = new Pessoa("Kayan", "kayan@gmail.com", 21);
+        Connection conexao = FabricaConexao.getConnection();
+        String sql = "UPDATE Pessoa SET Nome = (?) WHERE Cod = ?";
+        
+        PreparedStatement ps = conexao.prepareStatement(sql);
+        ps.setString(1, p.getNome());
+        ps.setInt(2, 2);
+        ps.execute();
+        
+        System.out.println("Success!");
+
+        conexao.close();
+    }
+}
